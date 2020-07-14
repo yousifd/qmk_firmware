d0d09be7f599d235977b05d1ab799e88c8fc591f
Yousif Aldolaijan <yad999@gmail.com>
2020-07-14 12:46:49 +0300

    added console support + enabled via to kbd75

--- keyboards/kbdfans/kbd75/rev1/rules.mk
+++ keyboards/kbdfans/kbd75/rev1/rules.mk
@@ -17,12 +17,15 @@ BOOTLOADER = atmel-dfu
 BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes	# Mouse keys
 EXTRAKEY_ENABLE = yes	# Audio control and System control
-CONSOLE_ENABLE = no	# Console for debug
+CONSOLE_ENABLE = yes	# Console for debug
 COMMAND_ENABLE = no    # Commands for debug and configuration
 SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = yes  # Enable keyboard backlight functionality
 AUDIO_ENABLE = no
 RGBLIGHT_ENABLE = yes
+
+VIA_ENABLE = yes
+LINK_TIME_OPTIMIZATION_ENABLE = yes
 
 LAYOUTS = 75_ansi 75_iso
